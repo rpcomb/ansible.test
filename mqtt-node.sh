2d6a6ef2b3134e0dcefd0480ec81033fe71e3f87
Rene <rcomb81@gmail.com>
2024-10-31 11:22:44 -0500

    Update mqtt-node.sh

--- mqtt-node.sh
+++ mqtt-node.sh
@@ -24,4 +24,4 @@ sudo docker volume create portainer_data &&
 
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.4 &&
 
-curl -fsSL https://tailscale.com/install.sh && sudo tailscale up
+sudo curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up
